ecbd0fbd1e9fab3c49c7eb57c4030ac8b98bb798
Francisco Guerra <paco.gq.0709@gmail.com>
2022-10-04 13:57:08 -0500

    Initial Commit

---
+++ AppNasa/AppNasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNasaApp.swift
+//  AppNasa
+//
+//  Created by Paco Guerra on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AppNasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
